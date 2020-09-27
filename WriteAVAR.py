cca7c822727b705986d675898186d9a80bd728b9
Sebastian Carewe <sebastian.carewe@gmail.com>
2020-09-27 21:44:55 +0200

    Cleanup

--- WriteAVAR.py
+++ WriteAVAR.py
@@ -1,21 +1,21 @@
-#MenuTitle: Write AVAR table
-# -*- coding: utf-8 -*-
-import vanilla
+	#MenuTitle: Write AVAR table
+	# -*- coding: utf-8 -*-
+	import vanilla
 
-__doc__="""
-Writes an AVAR table based on current weight distribution
-"""
+	__doc__="""
+	Writes an AVAR table based on current weight distribution
+	"""
 
-font = Glyphs.font
+	font = Glyphs.font
 
-axisMinimum = font.instances[0].weightValue # lightest stem weight
-axisRange = font.instances[-1].weightValue - axisMinimum # boldest stem weight
+	# create a dictionary which indexes all instances by weight
+	instancelist = sorted({font.instances[i].weightValue for i in range(len(font.instances))})
 
-# create a dictionary which indexes all instances by weight
-instancelist = sorted({font.instances[i].weightValue for i in range(len(font.instances))})
+	axisMinimum = instancelist[0] # lightest stem weight
+	axisRange = instancelist[-1] - axisMinimum # boldest stem weight
 
-# create a dictionary which stores the AVAR table
-avartable = {"wght": {int(axisRange/(len(instancelist)-1)*l+axisMinimum):instancelist[l] for l in range(len(instancelist))}}
+	# create a dictionary which stores the AVAR table
+	avartable = {"wght": {int(axisRange/(len(instancelist)-1)*l+axisMinimum):instancelist[l] for l in range(len(instancelist))}}
 
-# write AVAR table to custom parameters
-font.customParameters["Axis Mappings"] = avartable
+	# write AVAR table to custom parameters
+	font.customParameters["Axis Mappings"] = avartable
