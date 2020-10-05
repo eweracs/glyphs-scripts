662a636dc9f60fb01adce01c93d9b822bb3792bb
Sebastian Carewe <sebastian.carewe@gmail.com>
2020-10-05 17:55:40 +0200

    Append if parameter already existent

--- InterpolateKerning.py
+++ InterpolateKerning.py
@@ -12,6 +12,9 @@ font = Glyphs.font
 M1 = 30
 M2 = 20
 
+class1 = "@Uppercase"
+class2 = "@Uppercase"
+
 minWeight = font.masters[0].weightValue
 maxWeight = font.masters[-1].weightValue
 
@@ -20,5 +23,14 @@ weightRange = maxWeight - minWeight
 for instance in font.instances:
 
 	kern = float((M2 - M1) / weightRange * (instance.weightValue - minWeight)) + M1
-	instance.customParameters["Replace Feature"] = """kern;
-pos @Uppercase """ + str(int(kern)) + " @Uppercase;"
+
+	if instance.customParameters:
+
+		kernText = """{}
+pos {} {} {};""".format(instance.customParameters["Replace Feature"], class1, str(int(kern)), class2)
+
+	else:
+		kernText = """kern;
+pos {} {} {};""".format(class1, str(int(kern)), class2)
+
+	instance.customParameters["Replace Feature"] = kernText
