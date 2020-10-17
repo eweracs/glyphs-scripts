0f3c5f62abf5187caed9184750851b18dc5d7ce8
Sebastian Carewe <sebastian.carewe@gmail.com>
2020-10-17 20:31:06 +0200

    Update imports, variable names

--- WriteAVAR.py
+++ WriteAVAR.py
@@ -1,20 +1,20 @@
-#MenuTitle: Write AVAR table
+# MenuTitle: Write AVAR table
 # -*- coding: utf-8 -*-
-import vanilla
 
-__doc__="""
+__doc__ = """
 Writes an AVAR table based on current weight distribution
 """
 
-font = Glyphs.font
+font = Font
 
-# create a list which indexes all instances by weight
-instancelist = sorted({instance.weightValue for instance in font.instances})
+# create a list which indexes all instance weights
+instanceweights = sorted({instance.weightValue for instance in font.instances})
 
-axisMinimum = instancelist[0] # lightest stem weight
-axisRange = instancelist[-1] - axisMinimum # axis range 
+axisMinimum = instancelist[0]  # lightest stem weight
+axisRange = instancelist[-1] - axisMinimum  # axis range
 
 # calculate and write AVAR table to custom parameters
-font.customParameters["Axis Mappings"] = {"wght": {int(axisRange/(len(instancelist)-1)*l+axisMinimum):instancelist[l] for l in range(len(instancelist))}}
+font.customParameters["Axis Mappings"] = {"wght": {int(axisRange/(len(instanceweights) - 1)*instance + axisMinimum): instanceweights[instance] for instance in range(len(instancelist))}}
 
-Glyphs.showNotification(title="Wrote AVAR table", message="Table calculated for " + str(len(instancelist)) + " instances")
+Glyphs.showNotification(title="Wrote AVAR table",
+                        message="Table calculated for " + str(len(instanceweights)) + " instances")
