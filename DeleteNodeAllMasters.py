9ae22ba393ab152d3b983525eb46f844c89eda85
Sebastian Carewe <sebastian.carewe@gmail.com>
2022-02-01 16:19:56 +0000

    Fix iteration

--- DeleteNodeAllMasters.py
+++ DeleteNodeAllMasters.py
@@ -8,6 +8,9 @@ Deletes the selected node(s) in all masters.
 for glyph in Font.selectedLayers:
 	for layer in glyph.parent.layers:
 		for path in layer.paths:
+			delete_nodes = []
 			for node in path.nodes:
 				if node.selected:
-					path.removeNodeCheckKeepShape_(node)
+					delete_nodes.append(node)
+			for delete_node in delete_nodes:
+				path.removeNodeCheckKeepShape_(delete_node)
