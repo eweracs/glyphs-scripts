10676f244ddd1e0bf8ccdd25707f1f5a2fb31819
Sebastian Carewe <sebastian.carewe@gmail.com>
2022-02-15 10:44:26 +0000

    Add Reset values

--- LetterspacerVisualiser.py
+++ LetterspacerVisualiser.py
@@ -24,6 +24,8 @@ class HTLSVisualiser:
 		self.font = Font
 
 		self.currentMasterId = self.font.selectedFontMaster.id
+		self.originalParams = []
+		self.force_update = False
 
 		for i, master in enumerate(Font.masters):
 			if not master.customParameters["paramArea"]:
@@ -31,6 +33,7 @@ class HTLSVisualiser:
 					Font.glyphs["idotless"].layers[i].LSB * master.xHeight / 100)
 			if not master.customParameters["paramDepth"]:
 				master.customParameters["paramDepth"] = 10
+			self.originalParams.append([master.customParameters["paramArea"], master.customParameters["paramDepth"]])
 
 		self.areaValueForMaster = int(self.font.selectedFontMaster.customParameters["paramArea"])
 		self.depthValueForMaster = int(self.font.selectedFontMaster.customParameters["paramDepth"])
@@ -46,7 +49,9 @@ class HTLSVisualiser:
 
 		self.ypos = 10
 
-		self.w.areaTtitle = vanilla.TextBox((10, self.ypos, -10, 14), "Area", sizeStyle="small")
+		self.w.areaTtitle = vanilla.TextBox((10, self.ypos, -10, 14), "Area (" + str(self.originalParams[
+			self.font.masterIndex][0]) + ")",
+		                                    sizeStyle="small")
 		self.w.areaEntry = vanilla.EditText((-60, self.ypos, -10, 19),
 		                                    self.areaValueForMaster,
 		                                    sizeStyle="small", callback=self.enter_area_value)
@@ -59,7 +64,8 @@ class HTLSVisualiser:
 
 		self.ypos += 30
 
-		self.w.depthTtitle = vanilla.TextBox((10, self.ypos, -10, 14), "Depth", sizeStyle="small")
+		self.w.depthTtitle = vanilla.TextBox((10, self.ypos, -10, 14), "Depth (" + str(self.originalParams[
+			self.font.masterIndex][1]) + ")", sizeStyle="small")
 		self.w.depthEntry = vanilla.EditText((-60, self.ypos, -10, 19),
 		                                    self.depthValueForMaster,
 		                                    sizeStyle="small", callback=self.enter_depth_value)
@@ -69,6 +75,10 @@ class HTLSVisualiser:
 		self.w.depthSlider = vanilla.Slider((10, self.ypos, -10, 23), minValue=1,
 		                                   maxValue=20,
 		                                   value=self.depthValueForMaster, callback=self.depth_slider_callback)
+
+		self.ypos += 30
+
+		self.w.resetButton = vanilla.Button((10, self.ypos, 100, 20), "Reset", callback=self.reset_values)
 
 		self.ypos += 30
 
@@ -108,23 +118,40 @@ class HTLSVisualiser:
 		self.w.depthEntry.set(int(sender.get()))
 		self.space_glyphs()
 
+	def reset_values(self, sender):
+		self.font.selectedFontMaster.customParameters["paramArea"] = int(self.originalParams[self.font.masterIndex][0])
+		self.font.selectedFontMaster.customParameters["paramDepth"] = int(self.originalParams[self.font.masterIndex][1])
+		self.force_update = True
+		self.ui_update(None)
+		self.force_update = False
+		self.space_glyphs()
+
+
 	def space_glyphs(self):
 		self.font.currentTab.textCursor = 0
 		self.font.currentTab.textRange = len(Font.currentTab.text)
-		HT_LetterSpacer_script.HTLetterspacerScript(ui=False, all_masters=False)
+		try:
+			HT_LetterSpacer_script.HTLetterspacerScript(ui=False, all_masters=False)
+		except:
+			HT_LetterSpacer_script.HTLetterspacerScript(ui=False)
 		for layer in self.font.selectedLayers:
 			layer.syncMetrics()
 
 	def ui_update(self, info):
-		if self.currentMasterId != self.font.selectedFontMaster.id:  # only perform the below if the master was switched
-			self.areaValueForMaster = int(self.font.selectedFontMaster.customParameters["paramArea"])
-			self.w.areaEntry.set(self.areaValueForMaster)
-			self.w.areaSlider.setMaxValue(self.areaValueForMaster * 1.5)
-			self.w.areaSlider.set(self.areaValueForMaster)
-			self.depthValueForMaster = int(self.font.selectedFontMaster.customParameters["paramDepth"])
-			self.w.depthEntry.set(self.depthValueForMaster)
-			self.w.depthSlider.set(self.depthValueForMaster)
-			self.currentMasterId = self.font.selectedFontMaster.id
+		if self.currentMasterId == self.font.selectedFontMaster.id and self.force_update is False:
+			return
+		print("h")
+		self.w.areaTtitle.set("Area (" + str(self.originalParams[self.font.masterIndex][0]) + ")")
+		self.areaValueForMaster = int(self.font.selectedFontMaster.customParameters["paramArea"])
+		self.w.areaEntry.set(self.areaValueForMaster)
+		self.w.areaSlider.setMaxValue(self.areaValueForMaster * 1.5)
+		self.w.areaSlider.set(self.areaValueForMaster)
+
+		self.w.depthTtitle.set("Depth (" + str(self.originalParams[self.font.masterIndex][1]) + ")")
+		self.depthValueForMaster = int(self.font.selectedFontMaster.customParameters["paramDepth"])
+		self.w.depthEntry.set(self.depthValueForMaster)
+		self.w.depthSlider.set(self.depthValueForMaster)
+		self.currentMasterId = self.font.selectedFontMaster.id
 
 	def window_close(self, sender):
 		Glyphs.removeCallback(self.ui_update, UPDATEINTERFACE)
