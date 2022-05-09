cb3a9f01c8141b89994852b658293d186c68b64d
Sebastian Carewe <sebastian.carewe@gmail.com>
2022-05-09 13:45:20 +0100

    Init

---
+++ Drawing/ToggleAutomaticAlignmentAllMasters.py
@@ -0,0 +1,27 @@
+# MenuTitle: Toggle Automatic Alignment for All Masters
+# -*- coding: utf-8 -*-
+
+__doc__ = """
+Toggles automatic alignment for the selected component in all masters.
+"""
+
+
+class ToggleAutomaticAlignmentAllMasters:
+	def __init__(self):
+		self.layer = Layer
+
+		if self.layer is None:
+			return
+
+		for i, reference in enumerate(self.layer.components):
+			if reference.selected:
+				reference.automaticAlignment = not reference.automaticAlignment
+
+			for layer in self.layer.parent.layers:
+				if layer is self.layer:
+					continue
+
+				layer.components[i].automaticAlignment = reference.automaticAlignment
+
+
+ToggleAutomaticAlignmentAllMasters()
