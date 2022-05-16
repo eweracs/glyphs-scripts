315ce2453a3f1ddec2395d040ddc073d87dd1ab7
Sebastian Carewe <sebastian.carewe@gmail.com>
2022-05-16 13:14:00 +0100

    Init

---
+++ Drawing/ToggleSpecialLayer.py
@@ -0,0 +1,31 @@
+# MenuTitle: Toggle Special layer
+# -*- coding: utf-8 -*-
+
+__doc__ = """
+Toggles the activation state of special layers.
+"""
+
+
+def toggle_special_layer(layer):
+	if layer.attributes:
+		layer.userData["attributes"] = {}
+		if "axisRules" in layer.attributes:
+			layer.userData["attributes"]["axisRules"] = {}
+			for axis in layer.attributes["axisRules"].keys():
+				layer.userData["attributes"]["axisRules"][axis] = {}
+				for rule in layer.attributes["axisRules"][axis].keys():
+					layer.userData["attributes"]["axisRules"][axis][rule] = layer.attributes["axisRules"][axis][rule]
+			del layer.attributes["axisRules"]
+		if "coordinates" in layer.attributes:
+			layer.userData["attributes"]["coordinates"] = {}
+			for axis in layer.attributes["coordinates"].keys():
+				layer.userData["attributes"]["coordinates"][axis] = layer.attributes["coordinates"][axis]
+			del layer.attributes["coordinates"]
+		layer.name = "(Deactivated)"
+	else:
+		for key in layer.userData["attributes"]:
+			layer.attributes[key] = layer.userData["attributes"][key]
+		del layer.userData["attributes"]
+
+for layer in Font.selectedLayers:
+	toggle_special_layer(layer)
