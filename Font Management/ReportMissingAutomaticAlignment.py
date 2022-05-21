cfbfd4af1660b600f87c54a263afc7f8c77b8b24
Sebastian Carewe <sebastian.carewe@gmail.com>
2022-05-21 11:09:35 +0100

    Init

---
+++ Font Management/ReportMissingAutomaticAlignment.py
@@ -0,0 +1,45 @@
+# MenuTitle: Report Missing Automatic Alignment
+# -*- coding: utf-8 -*-
+
+__doc__ = """
+Prints a tab with all layers with mixed paths/components, or more than one component with missing automatic alignment.
+"""
+
+from vanilla import *
+
+
+class ReportMissingAutomaticAlignment:
+	def __init__(self):
+		self.font = Font
+
+		if self.font is None:
+			Message("No font selected", "Select a font project!")
+			return
+
+		missing_alignment = []
+
+		for glyph in self.font.glyphs:
+			for layer in glyph.layers:
+				if not layer.components:
+					continue
+
+				# check for mixed paths/components
+				if layer.paths and layer.components:
+					missing_alignment.append(layer)
+					continue
+
+				# check for multiple components without automatic alignment
+				count = 0
+				for component in layer.components:
+					if component.automaticAlignment:
+						count += 1
+				if count < len(layer.components) - 1:
+					missing_alignment.append(layer)
+
+		if len(missing_alignment) > 0:
+			self.font.newTab(missing_alignment)
+		else:
+			Message("No layers with missing automatic alignment found.", "All good!", OKButton="Nice.")
+
+
+ReportMissingAutomaticAlignment()
