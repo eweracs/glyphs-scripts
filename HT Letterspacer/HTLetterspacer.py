922ca4999b28956628e7cad97e0585f8b7e0c151
Sebastian Carewe <sebastian.carewe@gmail.com>
2022-07-08 23:17:32 +0200

    Add HT Letterspacer scripts for HTLS Manager integration

---
+++ HT Letterspacer/HTLetterspacer.py
@@ -0,0 +1,54 @@
+# MenuTitle: HT Letterspacer
+# -*- coding: utf-8 -*-
+
+__doc__ = """
+Runs HT Letterspacer for the current selection. Hold Option to run for all masters.
+"""
+
+from Foundation import NSEvent
+
+import_success = False
+try:
+	from HTLSLibrary import *
+	import_success = True
+except:
+	Message("Please install HTLS Manager from the plugin manager and restart Glyphs.", "HTLS Manager required")
+
+
+class HTLS:
+	def __init__(self):
+		self.font = Font
+
+		if self.font is None:
+			Message("No font selected", "Select a font project!")
+			return
+
+		self.pressed_keys = NSEvent.modifierFlags()
+		self.option_key = 524288
+		self.option_key_pressed = self.pressed_keys & self.option_key == self.option_key
+
+		self.all_masters = False
+
+		if self.option_key_pressed:
+			self.all_masters = True
+
+		for glyph in self.font.selectedLayers:
+			parent = glyph.parent
+			for layer in parent.layers:
+				if not layer.isMasterLayer:
+					continue
+				if not self.all_masters and layer.associatedMasterId != self.font.selectedFontMaster.id:
+					continue
+
+				self.engine = HTLSEngine(layer)
+
+				layer_lsb, layer_rsb = self.engine.current_layer_sidebearings() or [None, None]
+				if not layer_lsb or not layer_rsb:
+					continue
+				layer.LSB, layer.RSB = layer_lsb, layer_rsb
+
+				print(self.engine.output)
+
+
+if import_success:
+	HTLS()
---
+++ HT Letterspacer/HTLetterspacerForAllMasters.py
@@ -0,0 +1,40 @@
+# MenuTitle: HT Letterspacer for All Masters
+# -*- coding: utf-8 -*-
+
+__doc__ = """
+Runs HT Letterspacer for the current selection in all masters.
+"""
+
+import_success = False
+try:
+	from HTLSLibrary import *
+	import_success = True
+except:
+	Message("Please install HTLS Manager from the plugin manager and restart Glyphs.", "HTLS Manager required")
+
+
+class HTLSAllMasters:
+	def __init__(self):
+		self.font = Font
+
+		if self.font is None:
+			Message("No font selected", "Select a font project!")
+			return
+
+		for glyph in self.font.selectedLayers:
+			parent = glyph.parent
+			for layer in parent.layers:
+				if not layer.isMasterLayer:
+					continue
+				self.engine = HTLSEngine(layer)
+
+				layer_lsb, layer_rsb = self.engine.current_layer_sidebearings() or [None, None]
+				if not layer_lsb or not layer_rsb:
+					continue
+				layer.LSB, layer.RSB = layer_lsb, layer_rsb
+
+				print(self.engine.output)
+
+
+if import_success:
+	HTLSAllMasters()
