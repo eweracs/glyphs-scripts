0ff3d9d31100c5ea176f471735e35a52f353d4b3
Sebastian Carewe <sebastian.carewe@gmail.com>
2022-07-18 15:08:10 +0200

    Fix continue when sb = 0

--- HT Letterspacer/HTLetterspacer.py
+++ HT Letterspacer/HTLetterspacer.py
@@ -43,7 +43,7 @@ class HTLS:
 				self.engine = HTLSEngine(layer)
 
 				layer_lsb, layer_rsb = self.engine.current_layer_sidebearings() or [None, None]
-				if not layer_lsb or not layer_rsb:
+				if (not layer_lsb or not layer_rsb) and (layer_lsb != 0 or layer_rsb != 0):
 					continue
 				layer.LSB, layer.RSB = layer_lsb, layer_rsb
 
