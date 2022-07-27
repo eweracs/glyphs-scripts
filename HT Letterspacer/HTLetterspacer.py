c5558d13d13d33dbac5a89e7398a840cd4ed17e4
Sebastian Carewe <sebastian.carewe@gmail.com>
2022-07-27 11:30:22 +0200

    Run script from Library module

--- HT Letterspacer/HTLetterspacer.py
+++ HT Letterspacer/HTLetterspacer.py
@@ -14,42 +14,9 @@ try:
 except:
 	Message("Please install HTLS Manager from the plugin manager and restart Glyphs.", "HTLS Manager required")
 
-
-class HTLS:
-	def __init__(self):
-		self.font = Font
-
-		if self.font is None:
-			Message("No font selected", "Select a font project!")
-			return
-
-		self.pressed_keys = NSEvent.modifierFlags()
-		self.option_key = 524288
-		self.option_key_pressed = self.pressed_keys & self.option_key == self.option_key
-
-		self.all_masters = False
-
-		if self.option_key_pressed:
-			self.all_masters = True
-
-		for glyph in self.font.selectedLayers:
-			parent = glyph.parent
-			for layer in parent.layers:
-				if not layer.isMasterLayer:
-					continue
-				if not self.all_masters and layer.associatedMasterId != self.font.selectedFontMaster.id:
-					continue
-
-				self.engine = HTLSEngine(layer)
-
-				layer_lsb, layer_rsb = self.engine.current_layer_sidebearings() or [None, None]
-				if (not layer_lsb or not layer_rsb) and (layer_lsb != 0 or layer_rsb != 0):
-					continue
-				layer.LSB, layer.RSB = layer_lsb, layer_rsb
-				layer.syncMetrics()
-
-				print(self.engine.output)
-
-
 if import_success:
-	HTLS()
+	pressed_keys = NSEvent.modifierFlags()
+	option_key = 524288
+	option_key_pressed = pressed_keys & option_key == option_key
+
+	HTLSScript(option_key_pressed)
--- HT Letterspacer/HTLetterspacerForAllMasters.py
+++ HT Letterspacer/HTLetterspacerForAllMasters.py
@@ -13,29 +13,5 @@ except:
 	Message("Please install HTLS Manager from the plugin manager and restart Glyphs.", "HTLS Manager required")
 
 
-class HTLSAllMasters:
-	def __init__(self):
-		self.font = Font
-
-		if self.font is None:
-			Message("No font selected", "Select a font project!")
-			return
-
-		for glyph in self.font.selectedLayers:
-			parent = glyph.parent
-			for layer in parent.layers:
-				if not layer.isMasterLayer:
-					continue
-				self.engine = HTLSEngine(layer)
-
-				layer_lsb, layer_rsb = self.engine.current_layer_sidebearings() or [None, None]
-				if (not layer_lsb or not layer_rsb) and (layer_lsb != 0 or layer_rsb != 0):
-					continue
-				layer.LSB, layer.RSB = layer_lsb, layer_rsb
-				layer.syncMetrics()
-
-				print(self.engine.output)
-
-
 if import_success:
-	HTLSAllMasters()
+	HTLSScript(True)
