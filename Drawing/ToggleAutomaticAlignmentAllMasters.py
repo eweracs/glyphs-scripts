d22bb39f06209618af9ba1f7cc7fc4028f7d719e
Sebastian Carewe <sebastian.carewe@gmail.com>
2022-11-01 19:00:44 +0100

    Allow multiple layer selection

--- Drawing/ToggleAutomaticAlignmentAllMasters.py
+++ Drawing/ToggleAutomaticAlignmentAllMasters.py
@@ -7,20 +7,19 @@ Toggles automatic alignment for the selected component in all masters.
 
 
 def toggle_automatic_alignment_all_masters():
-	layer = Layer
+	for layer in Font.selectedLayers:
+		if layer is None:
+			return
 
-	if layer is None:
-		return
+		for i, reference in enumerate(layer.components):
+			if reference.selected or len(layer.selection) == 0:
+				reference.automaticAlignment = not reference.automaticAlignment
 
-	for i, reference in enumerate(layer.components):
-		if reference.selected or len(layer.selection) == 0:
-			reference.automaticAlignment = not reference.automaticAlignment
+			for layer in layer.parent.layers:
+				if layer is layer:
+					continue
 
-		for layer in layer.parent.layers:
-			if layer is layer:
-				continue
-
-			layer.components[i].automaticAlignment = reference.automaticAlignment
+				layer.components[i].automaticAlignment = reference.automaticAlignment
 
 
 toggle_automatic_alignment_all_masters()
