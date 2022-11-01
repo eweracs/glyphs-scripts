736b2c8bb02e2f4b4f631aac485b24b05fac91aa
Sebastian Carewe <sebastian.carewe@gmail.com>
2022-11-01 18:58:38 +0100

    Toggle all if nothing selected

--- Drawing/ToggleAutomaticAlignmentAllMasters.py
+++ Drawing/ToggleAutomaticAlignmentAllMasters.py
@@ -6,22 +6,21 @@ Toggles automatic alignment for the selected component in all masters.
 """
 
 
-class ToggleAutomaticAlignmentAllMasters:
-	def __init__(self):
-		self.layer = Layer
+def toggle_automatic_alignment_all_masters():
+	layer = Layer
 
-		if self.layer is None:
-			return
+	if layer is None:
+		return
 
-		for i, reference in enumerate(self.layer.components):
-			if reference.selected:
-				reference.automaticAlignment = not reference.automaticAlignment
+	for i, reference in enumerate(layer.components):
+		if reference.selected or len(layer.selection) == 0:
+			reference.automaticAlignment = not reference.automaticAlignment
 
-			for layer in self.layer.parent.layers:
-				if layer is self.layer:
-					continue
+		for layer in layer.parent.layers:
+			if layer is layer:
+				continue
 
-				layer.components[i].automaticAlignment = reference.automaticAlignment
+			layer.components[i].automaticAlignment = reference.automaticAlignment
 
 
-ToggleAutomaticAlignmentAllMasters()
+toggle_automatic_alignment_all_masters()
