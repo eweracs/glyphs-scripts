ecb9747562d5eef66db7e3229c6f27254ddff25d
Sebastian Carewe <51919489+eweracs@users.noreply.github.com>
2023-07-01 14:07:10 +0200

    Add Paste backup layer to master background

---
+++ Drawing/PasteBackupLayerToMasterBackground.py
@@ -0,0 +1,14 @@
+# MenuTitle: Paste Backup Layer to Master Background
+# -*- coding: utf-8 -*-
+
+__doc__ = """
+Pastes the contents of the selected backup layer to the associated master layer background.
+"""
+
+master_layer = Layer.parent.layers[Layer.associatedMasterId]
+if not Layer.isMasterLayer and not Layer.isSpecialLayer:
+	master_layer.background.shapes = Layer.shapes.copy()
+	master_layer.background.hints = Layer.hints.copy()
+	master_layer.background.anchors = Layer.anchors.copy()
+else:
+	Message(title="Not a backup layer", message="Please select a backhup layer from which to copy the contents.")
