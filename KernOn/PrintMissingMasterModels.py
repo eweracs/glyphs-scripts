f3ca8e9e44e01fa38ca1bd90596043cb74ea587a
eweracs <sebastian.carewe@gmail.com>
2024-10-03 19:56:39 +0200

    Fix crash when encountering autokerned masters

--- KernOn/PrintMissingMasterModels.py
+++ KernOn/PrintMissingMasterModels.py
@@ -15,14 +15,18 @@ if Font is None:
 all_models = []
 
 for master in Font.masters:
-	if master.userData["KernOnAutoKernedFrom"] or master.userData["KernOnIsInterpolated"]:
+	if master.userData["KernOnAutoKernedFrom"] or master.userData["KernOnIsInterpolated"] or not master.userData["KernOnModels"]:
+		continue
+	if not len(master.userData["KernOnModels"]):
 		continue
 	for model in master.userData["KernOnModels"]:
 		if model not in all_models:
 			all_models.append(model)
 
 for master in Font.masters:
-	if master.userData["KernOnAutoKernedFrom"] or master.userData["KernOnIsInterpolated"]:
+	if master.userData["KernOnAutoKernedFrom"] or master.userData["KernOnIsInterpolated"] or not master.userData["KernOnModels"]:
+		continue
+	if not len(master.userData["KernOnModels"]):
 		continue
 	missing_models = []
 	for model in all_models:
