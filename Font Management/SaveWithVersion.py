807e1ad8603ed8dfa968dd3739208172971d68b8
Sebastian Carewe <51919489+eweracs@users.noreply.github.com>
2025-08-14 21:46:27 +0200

    Naming simplification

--- Font Management/SaveWithVersion.py
+++ Font Management/SaveWithVersion.py
@@ -5,17 +5,6 @@ __doc__ = """
 Saves a copy of the current file as a .glyphs file with a version number.
 """
 
-italic_masters = 0
-for master in Glyphs.font.masters:
-    if master.italicAngle:
-        italic_masters += 1
+new_filepath = "%s %s.glyphs" % (".".join(Font.filepath.split(".")[:-1]), Glyphs.font.version())
 
-is_italic = ""
-if italic_masters:
-    is_italic = "Italic "
-
-new_filename = "%s %s%s.glyphs" % (Glyphs.font.familyName, is_italic, Glyphs.font.version())
-
-new_filepath = Font.filepath.replace(Glyphs.font.filepath.split("/")[-1], new_filename)
-
-Glyphs.font.save(new_filepath, makeCopy=True)
+Font.save(new_filepath, makeCopy=True)
